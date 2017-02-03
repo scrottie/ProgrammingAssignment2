6836decd9806190d9ca7da1f2eb6746490acb811
Scott Walters <scott@slowass.net>
2017-02-03 14:02:06 -0800

    Coursera R Programming homework assignment, week 3 or so

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix() returns an object to contain a matrix and cache a computed inverse of it as computed by cacheSolve()
+# cacheSolve() method to solve inverse matrices and cache the result in an object created by makeCacheMatrix()
 
-## Write a short comment describing this function
+# makeCacheMatrix
+
+# Returns an object (apparently this is what R objects look like) with set, get, set_cached_inverse_matrix, and get_cached_inverse_matrix methods.
+
+# set() stores an unsolved matrix.  This method also clears out any cached solved matrix.
+# get() fetches that same unsolved matrix.
+# set_cached_inverse_matrix() stores a solved matrix.
+# get_cached_inverse_matrix() fetches the stored, solved matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cached_inverse_matrix <- NULL
+        set <- function(y) {
+                x <<- y
+                cached_matrix <<- NULL
+        }
+        get <- function() x
+        set_cached_inverse_matrix <- function(mat) cached_inverse_matrix <<- mat
+        get_cached_inverse_matrix<- function() cached_inverse_matrix
+        list(set = set, get = get,
+             set_cached_inverse_matrix = set_cached_inverse_matrix,
+             get_cached_inverse_matrix = get_cached_inverse_matrix)
 }
 
+# cacheSolve
 
-## Write a short comment describing this function
+# Takes an object created by makeCachMatrix() and returns the solved matrix, employing caching.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cached_inverse_matrix <- x$get_cached_inverse_matrix()
+        if(!is.null(cached_inverse_matrix)) {
+                message("getting cached data")
+                return(cached_inverse_matrix)
+        }
+        original_matrix <- x$get()
+        m <- solve(original_matrix, ...)
+        x$set_cached_inverse_matrix(m)
+        m
 }
+
+# Example usage:
+
+m1 <- makeCacheMatrix( matrix(data = sapply(1:9, function(x) runif(1) ), nrow = 3, ncol = 3) )
+cacheSolve(m1)
+
+
